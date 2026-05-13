6ca72577b8045afd6c6a44dbb1f5c079a3307508
Mahaa9999 <qtr1617000@gmail.com>
2026-05-13 13:32:20 +0300

    Update connection.php
    Fix: connection and database compatibility

--- db/connection.php
+++ db/connection.php
@@ -1,18 +1,13 @@
 <?php
-// Database Configuration
 $servername = "localhost";
-$username = "root";     // Default username for local servers
-$password = "";         // Default password is empty
-$dbname = "recipe_app_database"; // The name we chose in database.sql
+$username = "root";       
+$password = "";           
+$dbname = "recipe_app_db"; 
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-// Set charset to UTF-8 to support all characters
-$conn->set_charset("utf8");
+$conn->set_charset("utf8mb4");
 ?>
